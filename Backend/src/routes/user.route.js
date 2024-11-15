a0794825e5bc3aa3a7c7abd14ac018b42e43eb98
rohandiaz <rohanamylasedas@gmail.com>
2024-11-15 23:01:19 +0530

    Multer injected

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -6,12 +6,13 @@ import {
     updateProfile
 } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/auth.js";
+import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post(singleUpload,register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
-router.route("/profile/update").post(isAuthenticated, updateProfile);
+router.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
 
 export default router;
