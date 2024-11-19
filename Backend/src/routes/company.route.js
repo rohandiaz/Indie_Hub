91cbfb8b4db18c05f12a30de13c2283dee3e16d2
rohandiaz <rohanamylasedas@gmail.com>
2024-11-19 22:39:23 +0530

    Admin Side work started

--- Backend/src/routes/company.route.js
+++ Backend/src/routes/company.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import isAuthenticated from "../middlewares/auth.js";
 import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
+import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
@@ -9,6 +10,6 @@ const router = express.Router();
 router.route("/register").post(isAuthenticated,registerCompany);
 router.route("/get").get(isAuthenticated,getCompany);
 router.route("/get/:id").get(isAuthenticated,getCompanyById);
-router.route("/update/:id").put(isAuthenticated,updateCompany);
+router.route("/update/:id").put(isAuthenticated,singleUpload,updateCompany);
 
 export default router;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,14 +5,19 @@ import App from './App.jsx'
 import { Toaster } from 'sonner'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
-import { persistStore } from 'redux-persist'
+import persistStore from 'redux-persist/es/persistStore'
+import { PersistGate } from 'redux-persist/integration/react'
+
+
+const persistor = persistStore(store);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
     </Provider>
-
-    <Toaster />
   </StrictMode>,
 )
