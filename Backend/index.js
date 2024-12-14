85b53e0a4054875215da047b696f030156daad1c
rohandiaz <rohanamylasedas@gmail.com>
2024-12-15 01:15:57 +0530

    index.js changed

--- Backend/index.js
+++ Backend/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://indie-hub-1.onrender.com',
     credentials:true
 }
 
